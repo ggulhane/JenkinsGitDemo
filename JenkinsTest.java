c0f643a7139cb0ef9c71fea013da6505b68a18da
ggulhane <gbg217@gmail.com>
2021-07-12 22:56:01 +0530

    second commit

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -5,6 +5,7 @@ public class JenkinsTest {
 	public static void main(String[] args) {
 		String s = "This project is created for Jenkins Practise";
 		System.out.println(s);
+		System.out.println("Second Commit");
 	}
 
 }
