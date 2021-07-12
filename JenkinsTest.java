bd58db05b3c3a99405f10830cbd05cab974cb7c3
ggulhane <gbg217@gmail.com>
2021-07-12 22:40:56 +0530

    first - for jenkins practice

---
+++ JenkinsTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		String s = "This project is created for Jenkins Practise";
+		System.out.println(s);
+	}
+
+}
